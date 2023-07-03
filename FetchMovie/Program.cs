b6c6254e4da3c6a803b15aa622668c0eab9a55fd
jonty472 <azttorrs@gmail.com>
2023-07-03 22:19:25 +1200

    Can now do a GET request, but need to work out how to deserialize the request and populate my Movie object

--- FetchMovie/Program.cs
+++ FetchMovie/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FetchMovie;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+internal class Movie
+{
+    public string MovieTitle { get; set; }
+    public int ReleaseDate { get; set; }
+}
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("What movie are you looking for: ");
+        string MovieTitle = Console.ReadLine();
+        Task<Movie> movieTask = RequestMovieAsync(MovieTitle);
+        Movie movie = await movieTask;
+        Console.WriteLine("Found movie");
+    }
+
+
+
+    private static async Task<Movie> RequestMovieAsync(string MovieTitle)
+    {
+        Console.WriteLine("Searching for movie");
+        // use api to lookup movie
+        using HttpClient client = new HttpClient();
+        HttpResponseMessage response = await client.GetAsync("https://api.themoviedb.org/3/search/movie?api_key=4cc1b68a07fe5ba265950e85ac96cb2c&query=Oldboy&year=2003");
+        response.EnsureSuccessStatusCode();
+        string requestBody = await response.Content.ReadAsStringAsync();
+        var movietitle = JsonSerializer.Deserialize<Movie>(requestBody.ToString()).Results.title;
+        // display movie details
+        Console.WriteLine($"test{movietitle}");
+        return new Movie();
+    }
+}
+
+
---
+++ FetchMovie/RequestMovie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * MVP -
+ * fetch movie details requested
+ * display movie details
+ */
+namespace FetchMovie
+{
+}
